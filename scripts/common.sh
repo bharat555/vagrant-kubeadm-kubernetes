5459fb5836c2e9d0dba0aa633c4c5adeb1a07f3e
Bharat <bharat555@users.noreply.github.com>
2022-04-03 13:35:23 +0530

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -17,30 +17,30 @@ sudo yum-config-manager \
     https://download.docker.com/linux/centos/docker-ce.repo
 	
 #sudo sed -i 's/$releasever/8/g' /etc/yum.repos.d/docker-ce.repo
-sudo dnf install -y containerd.io
+# sudo dnf install -y containerd.io
 
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
-overlay
-br_netfilter
-EOF
+# cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+# overlay
+# br_netfilter
+# EOF
 
-sudo modprobe overlay
-sudo modprobe br_netfilter
+# sudo modprobe overlay
+# sudo modprobe br_netfilter
 
-# Setup required sysctl params, these persist across reboots.
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-EOF
+# # Setup required sysctl params, these persist across reboots.
+# cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+# net.bridge.bridge-nf-call-iptables  = 1
+# net.ipv4.ip_forward                 = 1
+# net.bridge.bridge-nf-call-ip6tables = 1
+# EOF
 
-# Apply sysctl params without reboot
-sudo sysctl --system
+# # Apply sysctl params without reboot
+# sudo sysctl --system
 
-sudo mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
+# sudo mkdir -p /etc/containerd
+# containerd config default | sudo tee /etc/containerd/config.toml
 
-sudo systemctl restart containerd
+# sudo systemctl restart containerd
 
 
 # Create the .conf file to load the modules at bootup
